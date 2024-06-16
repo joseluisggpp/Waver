6e0814be3d1809382ceb363b13ed8ec1948f4668
José <kratos1054@gmail.com>
2024-06-16 15:32:13 +0200

    Añadido menú hamburguesa

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,33 +1,69 @@
-        <header>
-            <div class="header-container">
-                <div></div>
-                <div class="logo">
-                    <!--Aquí viene el logo-->
-                    <a href="{{ route('home') }}"><img src="{{ asset('media/logo_waver.png') }}"></a>
-                </div>
+<header>
+    <div class="header-container">
+        <div></div>
+        <div class="logo">
+            <!-- Aquí viene el logo -->
+            <a href="{{ route('home') }}"><img src="{{ asset('media/logo_waver.png') }}"></a>
+        </div>
 
-                <div class="header-right-corner">
-                    <!--Aquí vienen los 3 iconos de la esquina superior derecha-->
+        <div class="header-right-corner">
+            <!-- Aquí vienen los 3 iconos de la esquina superior derecha -->
 
-                    <a href="{{ route('login') }}"><img src="{{ asset('media/usuario.png') }}"></a>
-                    <a href="{{ route('cart') }}"><img src="{{ asset('media/carrito-de-compras.png') }}"></a>
+            <a href="{{ route('login') }}"><img src="{{ asset('media/usuario.png') }}"></a>
+            <a href="{{ route('cart') }}"><img src="{{ asset('media/carrito-de-compras.png') }}"></a>
+            <button id="showBurger">
+                <img src="{{ asset('media/burger.png') }}" alt="Menu">
+            </button>
+        </div>
+    </div>
+    <nav>
+        <ul>
+            <a href="{{ route('top10songs') }}" class="href">
+                <li><strong>Top 10 Canciones</strong></li>
+            </a>
+            <a href="{{ route('detection') }}" class="href">
+                <li><strong>Detección de canción</strong></li>
+            </a>
+            <a href="{{ route('contact') }}" class="href">
+                <li><strong>Contacto</strong></li>
+            </a>
+            <a href="{{ route('about-us') }}" class="href">
+                <li><strong>Sobre Nosotros</strong></li>
+            </a>
+        </ul>
+    </nav>
+    <div class="mobile-nav-items" id="mobileNavItems">
+        <button id="closeBurger">
+            <img src="{{ asset('media/boton-cerrar.png') }}" alt="Close">
+        </button>
+        <ul>
+            <a href="{{ route('top10songs') }}" class="href">
+                <li><strong>Top 10 Canciones</strong></li>
+            </a>
+            <a href="{{ route('detection') }}" class="href">
+                <li><strong>Detección de canción</strong></li>
+            </a>
+            <a href="{{ route('contact') }}" class="href">
+                <li><strong>Contacto</strong></li>
+            </a>
+            <a href="{{ route('about-us') }}" class="href">
+                <li><strong>Sobre Nosotros</strong></li>
+            </a>
+        </ul>
+    </div>
+</header>
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const showBurgerButton = document.getElementById('showBurger');
+        const closeBurgerButton = document.getElementById('closeBurger');
+        const mobileNavItems = document.getElementById('mobileNavItems');
 
-                </div>
-            </div>
-            <nav>
-                <ul>
-                    <a href="{{ route('top10songs') }}" class="href">
-                        <li><strong>Top 10 Canciones</strong></li>
-                    </a>
-                    <a href="{{ route('detection') }}" class="href">
-                        <li><strong>Detección de canción</strong></li>
-                    </a>
-                    <a href="{{ route('contact') }}" class="href">
-                        <li><strong>Contacto</strong></li>
-                    </a>
-                    <a href="{{ route('about-us') }}" class="href">
-                        <li><strong>Sobre Nosotros</strong></li>
-                    </a>
-                </ul>
-            </nav>
-        </header>
+        showBurgerButton.addEventListener('click', function() {
+            mobileNavItems.classList.add('show');
+        });
+
+        closeBurgerButton.addEventListener('click', function() {
+            mobileNavItems.classList.remove('show');
+        });
+    });
+</script>
